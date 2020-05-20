d367564da50e4f83f215206012e613e8864ea76e
Yan <chysft@gmail.com>
2020-05-20 19:09:44 +0800

    Update Card.java
    rank: char->int

--- app/src/main/java/com/app/boardgame4521/Card.java
+++ app/src/main/java/com/app/boardgame4521/Card.java
@@ -3,15 +3,15 @@ package com.app.boardgame4521;
 import com.app.boardgame4521.enumm.Suit;
 
 public class Card {
-    private char rank;
+    private int rank;
     private Suit suit;
 
-    public Card(char rank, Suit suit){
+    public Card(int rank, Suit suit){
         this.rank = rank;
         this.suit = suit;
     }
 
-    public char getRank() { return rank; }
+    public int getRank() { return rank; }
     public Suit getSuit() { return suit; }
 }
 
