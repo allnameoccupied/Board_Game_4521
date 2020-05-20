aadffd7a29fee167053b444046c3bf1aa2fe31ad
chysf <chysft@gmail.com>
2020-05-20 19:34:00 +0800

    card function

--- app/src/main/java/com/app/boardgame4521/Card.java
+++ app/src/main/java/com/app/boardgame4521/Card.java
@@ -3,11 +3,13 @@ package com.app.boardgame4521;
 import com.app.boardgame4521.enumm.Suit;
 
 public class Card {
-    int rank;
-    Suit suit;
+    private int rank;
+    private Suit suit;
     public Card(int rank, Suit suit){
         this.rank = rank;
         this.suit = suit;
     }
+    public int getRank() { return rank; }
+    public Suit getSuit() { return suit; }
 }
 
