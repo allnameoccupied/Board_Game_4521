36b91ce855fd9e4cabf33ca3ed77cb0398cb024f
max12345 <max98tkc@gmail.com>
2020-05-26 02:16:55 +0800

    from max

--- app/src/main/java/com/app/boardgame4521/Room_Selection.java
+++ app/src/main/java/com/app/boardgame4521/Room_Selection.java
@@ -1,13 +1,31 @@
 package com.app.boardgame4521;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.app.boardgame4521.MaxUtil.util;
+import com.app.boardgame4521.MaxUtil.utilGoogle;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Room_Selection extends AppCompatActivity {
     //custom view model
@@ -22,15 +40,128 @@ public class Room_Selection extends AppCompatActivity {
     }
 
     //func
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.room_selection_main);
 
+        //get view model
+        roomSelection_viewModel = new ViewModelProvider(this).get(RoomSelection_ViewModel.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.room_selection_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.room_selection_menu_item1:
+                util.log("add icon pressed");
+                break;
+            case R.id.room_selection_menu_item2:
+                if (Room_Selection_Frag1.thisFrag!=null){
+                    Room_Selection_Frag1.thisFrag.updateRoomList();
+                }
+                util.log("refresh icon pressed");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //Fragment 1    (display room for user to choose)
     public static class Room_Selection_Frag1 extends Fragment {
+        //var
+        public static Room_Selection_Frag1 thisFrag = null;
+        private RoomSelection_ViewModel viewModel;
+        private NavController navController;
+        private View view;
+        public ArrayList<RS_Room> roomList = new ArrayList<>();
 
+        //func
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            viewModel = new ViewModelProvider(this).get(RoomSelection_ViewModel.class);
+            thisFrag = this;
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            navController = NavHostFragment.findNavController(this);
+
+            //set recycler view
+            updateRoomList();
+        }
+
+        public void updateRoomList(){
+            RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.roomSelection_frag1_recycler);
+            utilGoogle.getFirestore().collection("active_room").get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()){
+                            for (DocumentSnapshot documentSnapshot : task.getResult()){
+                                roomList.add(new RS_Room(documentSnapshot));
+                            }
+                        }
+                    });
+            RS_RecyclerView_Adapter adapter = new RS_RecyclerView_Adapter(getContext(),roomList);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setAdapter(adapter);
+        }
+
+        //custom room class
+        public static class RS_Room{
+            //var
+
+            //func
+            public RS_Room(DocumentSnapshot roomData){
+
+            }
+        }
+
+        //custom adapter for recycler view
+        public static class RS_RecyclerView_Adapter
+                extends RecyclerView.Adapter<RS_RecyclerView_Adapter.RS_RecyclerView_ViewHolder>{
+            private Context context;
+            private ArrayList<RS_Room> roomList;
+
+            public RS_RecyclerView_Adapter(Context context, ArrayList<RS_Room> roomList){
+                this.context = context;
+                this.roomList = roomList;
+            }
+
+            @Override
+            public RS_RecyclerView_ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(context).inflate(R.layout.rs_recyclerview_item,parent,false);
+                RS_RecyclerView_ViewHolder holder = new RS_RecyclerView_ViewHolder(view);
+                //set item in holder to corr. view
+                return holder;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RS_RecyclerView_ViewHolder holder, int position) {
+                RS_Room room = roomList.get(position);
+                //set holder item to real data
+            }
+
+            @Override
+            public int getItemCount() {
+                return roomList.size();
+            }
+
+            //custom view holder for each item
+            public static class RS_RecyclerView_ViewHolder extends RecyclerView.ViewHolder{
+                //var
+
+                //func
+                public RS_RecyclerView_ViewHolder(@NonNull View itemView) {
+                    super(itemView);
+                }
+            }
+        }
     }
 }
