f66db20e965a44a96e2084c1b68138ea7cb86902
max12345 <max98tkc@gmail.com>
2020-05-26 23:40:36 +0800

    from max

--- app/src/main/java/com/app/boardgame4521/MaxUtil/utilGoogle.java
+++ app/src/main/java/com/app/boardgame4521/MaxUtil/utilGoogle.java
@@ -1,3 +1,9 @@
+/**
+ * # COMP 4521    #  Chan Hung Yan        20423715          hychanbr@connect.ust.hk
+ * # COMP 4521    #  Tai Ka Chun          20433540          kctaiab@connect.ust.hk
+ * # COMP 4521    #  Mong Kin Ip          20433629          kimong@connect.ust.hk
+ */
+
 package com.app.boardgame4521.MaxUtil;
 
 import android.content.Context;
